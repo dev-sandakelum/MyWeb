a6bd917be79702af5c29fd299761d193018eb598
developMEnew0X <165547551+developMEnew0X@users.noreply.github.com>
2025-01-30 18:17:28 +0530

    13

--- components/navbar/Navbar.jsx
+++ components/navbar/Navbar.jsx
@@ -47,11 +47,11 @@ export default function Navbar({colorCollection ,route ,setRoute}) {
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex justify-between h-16">
             <div className="flex items-center">
-              <Link href="/" className="flex-shrink-0 flex items-center">
+              <Button href="/" className="flex-shrink-0 flex items-center shadow-none" onClick={()=>{setRoute(1)}}>
                 <span className="text-xl font-bold text-gray-900">
                   Hasitha_San_
                 </span>
-              </Link>
+              </Button>
             </div>
             <div className="hidden sm:ml-6 sm:flex sm:items-center">
               <div className="flex space-x-4">
