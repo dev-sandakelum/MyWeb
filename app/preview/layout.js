09ef3d62b271ed2e4844b851466d5da8b48899c0
developMEnew0X <165547551+developMEnew0X@users.noreply.github.com>
2025-02-01 19:24:00 +0530

    21

--- app/preview/layout.js
+++ app/preview/layout.js
@@ -16,7 +16,7 @@ const colorCollection = {
 export default function RootLayout({ children }) {
   return (
     <div className="flex flex-col min-h-screen bg-gray-100">
-      <Link href={'/'}>{'<<'}BACK</Link>
+      <Link href={'/'} className="p-1">{'<<'}BACK</Link>
       {children}
     </div>
   );
