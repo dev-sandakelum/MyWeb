7a2f47a140cfc3fd53bc75375d2612f78d1d3154
developMEnew0X <165547551+developMEnew0X@users.noreply.github.com>
2025-02-01 22:06:15 +0530

    35

---
+++ app/metaData.js
@@ -0,0 +1,42 @@
+export const Metadata = {
+  title: "Hasitha Sadakelum | Portfolio",
+  description:
+    "Explore the portfolio of Hasitha Sadakelum, a passionate developer specializing in Next.js, Three.js, and TypeScript.",
+  keywords:
+    "Hasitha Sadakelum, portfolio, web development, Next.js, Three.js, TypeScript, programming",
+  author: "Hasitha Sadakelum",
+  robots: "index, follow",
+  themeColor: "#000000",
+  openGraph: {
+    title: "Hasitha Sadakelum | Portfolio",
+    description:
+      "Explore the portfolio of Hasitha Sadakelum, a passionate developer specializing in Next.js, Three.js, and TypeScript.",
+    url: "https://hs00.vercel.app/",
+    type: "website",
+    images: [
+      {
+        url: "https://hs00.vercel.app/p.jpg",
+        width: 1200,
+        height: 1200,
+        alt: "Hasitha Sadakelum Portfolio",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Hasitha Sadakelum | Portfolio",
+    description:
+      "Explore the portfolio of Hasitha Sadakelum, a passionate developer specializing in Next.js, Three.js, and TypeScript.",
+    site: "@your_twitter_handle",
+    creator: "@your_twitter_handle",
+    images: ["https://yourwebsite.com/twitter-image.jpg"],
+  },
+  alternates: {
+    canonical: "https://yourwebsite.com",
+  },
+  manifest: "/site.webmanifest",
+  icons: {
+    icon: "/favicon.ico",
+    apple: "/apple-touch-icon.png",
+  },
+};
